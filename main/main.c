c4b0a8804d23d81c48d587155a867b8411eff497
jl0up <41552872+jl0up@users.noreply.github.com>
2025-04-22 16:46:16 +0200

    added Wifi connection

--- main/main.c
+++ main/main.c
@@ -9,12 +9,22 @@
 
 #include "chip_info.h"
 #include "delayed_restart.h"
+#include "wifi_station.h"
+
+
+
+/* Wifi connection parameters (wifi_station.c) */
+// #define EXAMPLE_ESP_WIFI_SSID      "" // CONFIG_ESP_WIFI_SSID
+// #define EXAMPLE_ESP_WIFI_PASS      "" //CONFIG_ESP_WIFI_PASSWORD
+// #define EXAMPLE_ESP_MAXIMUM_RETRY  3 // CONFIG_ESP_MAXIMUM_RETRY
+
+
 
 void app_main(void)
 {
     print_chip_information();
 
+    wifi_station_connect();
 
-    
     delayed_restart();
 }
