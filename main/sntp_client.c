6c2435060c54c3738872529eba551a68346ab1e0
jl0up <41552872+jl0up@users.noreply.github.com>
2026-01-30 20:02:25 +0100

    Update timezone setting in SNTP

--- main/sntp_client.c
+++ main/sntp_client.c
@@ -9,6 +9,6 @@ void initialize_sntp(void) {
     esp_sntp_setoperatingmode(ESP_SNTP_OPMODE_POLL);
     esp_sntp_setservername(0, "pool.ntp.org");
     esp_sntp_init();
-    setenv("TZ", "Europe/Paris", 1);
+    setenv("TZ", "CET-1CEST,M3.5.0/02:00,M10.5.0/03:00", 1); // "CET-1CEST,M3.5.0/02:00,M10.5.0/03:00" is equivalent to Europe/Paris
     tzset();
 }
