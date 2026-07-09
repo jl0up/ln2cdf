075e8db856ab4602fda2662a8fda60007e353b45
jl0up <41552872+jl0up@users.noreply.github.com>
2026-07-09 12:57:21 +0200

    Wifi connection: now unlimited retries every 10s

--- main/wifi_station.c
+++ main/wifi_station.c
@@ -12,8 +12,6 @@
 
 static const char *TAG = "wifi_station.c";
 
-static int s_retry_num = 0;
-
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
 
@@ -30,20 +28,15 @@ void event_handler(void* arg, esp_event_base_t event_base,
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGW(TAG,"connect to the AP fail");
+    } else if (event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGW(TAG, "WiFi disconnected, retrying...");
+        current_ip[0] = '\0';
+        vTaskDelay(pdMS_TO_TICKS(10000));
+        esp_wifi_connect();
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
         snprintf(current_ip, sizeof(current_ip), IPSTR, IP2STR(&event->ip_info.ip));
         ESP_LOGI(TAG, "got ip: %s", current_ip);
-        s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
